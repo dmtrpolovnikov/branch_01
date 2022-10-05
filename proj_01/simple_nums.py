5a8cfd231f2188a0738b9354928ae7026a13f984
Dmitry Polovnikov <dmtrpolovnikov.com>
2022-10-05 12:49:24 +0300

    unit tests added

--- proj_01/simple_nums.py
+++ proj_01/simple_nums.py
@@ -1,5 +1,10 @@
 import math
+import sys
+import unittest
 
+#=============================================================================#
+#                           Class of simple numbers                           #
+#=============================================================================#
 class prime_nums:
     # define class example array of simple nums. 1 is excepted, 2 is used by default
     __all_simple_numbers      = [2]
@@ -8,7 +13,7 @@ class prime_nums:
     # define constructor and begin estimations
     def __init__(self, a, b):
         self.__interval_evaluator__(a, b)
- 
+        
     # define the function that checks whether the arg is a simple number
     def __is_prime__ (self, x):
         assert x > 1, 'Wrong arg!'
@@ -19,10 +24,13 @@ class prime_nums:
         # consider that we have to check only simple multipliers
         for i in self.__all_simple_numbers:
             if x % i == 0:      # zero mod -> x is not simple, break the loop
-                break
-            elif i >= sqrtx:    # we didn't find integer divisor, the num is simple
+                #break
+                return False
+            elif i >= sqrtx and x not in self.__all_simple_numbers:    # we didn't find integer divisor, the num is simple
                 self.__all_simple_numbers.append(x)   # add it to arr of simple nums
-                break                           # then break the loop
+                #print('weve got')
+                return True
+                #break                           # then break the loop
 
     # define the function for checking interval of numbers
     def __interval_evaluator__(self, a, b):
@@ -33,6 +41,7 @@ class prime_nums:
         for i in range (2, b + 1):
             self.__is_prime__(i)
         self.__summator__(a, b)
+        return self.__all_simple_numbers
 
     # return sum of simple nums
     def __summator__(self, a, b):
@@ -43,9 +52,45 @@ class prime_nums:
 
         # sum the simple numbers
         self.__target_simple_numbers = [n for n in range(a, b) if n in self.__all_simple_numbers]
+        print(self.__target_simple_numbers)
+        return self.__target_simple_numbers
 
     def show_sum(self):
         print(sum(self.__target_simple_numbers))
+        return(sum(self.__target_simple_numbers))
+#=============================================================================#
 
-ff = prime_nums(22,36)
-ff.show_sum()
+
+
+#=============================================================================#
+#                          UnitTest Class                                     #
+#=============================================================================#
+class SN_Test(unittest.TestCase):
+    sn = prime_nums(2, 10)
+    def init_prime(self):
+        self.sn = prime_nums(2, 10)
+
+    def test_int_eval(self):
+        self.assertEqual(self.sn.__interval_evaluator__(2, 10), [2, 3, 5, 7])
+
+    def test_summator(self):
+        self.assertEqual(self.sn.__summator__(5, 10), [5, 7])
+#=============================================================================#
+
+
+
+
+#=============================================================================#
+#                               Main                                          #
+#=============================================================================# 
+if __name__ == '__main__':
+    
+    try:
+        a, b = int(sys.argv[1]), int(sys.argv[2])
+        unittest.main(a, b)
+        print('a = ', a, ' b = ', b)
+        s_nums = prime_nums(a, b)
+        s_nums.show_sum()
+    except:
+        print('Wrong args!')
+#=============================================================================#
